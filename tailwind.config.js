6775ce37da62b0537bbb8e22c0c289ba7094d590
Gerardo Camorlinga Jr. <gercamjr.dev@gmail.com>
2025-06-06 10:36:02 -0500

    scaffolding css

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cadmium: {
+          orange: "#EB8B2B",
+        },
+        smoky: {
+          black: "#180E04",
+        },
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        heading: ["Jakusty", "sans-serif"],
+        body: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
